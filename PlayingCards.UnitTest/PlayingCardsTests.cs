a32da4e3e34fa4367360ab40e04c43ed10385d91
LOUIS\LouisThomps <thompsonl0109@gmail.com>
2025-11-20 08:54:56 +0000

    added playing card tests

---
+++ PlayingCards.UnitTest/PlayingCardsTests.cs
@@ -0,0 +1,39 @@
+﻿namespace PlayingCards.UnitTest
+{
+    public class PlayingCardsTests
+    {
+        [Fact]
+        public void AcceptsListOfCards()
+        {
+
+        }
+
+        [Fact]
+        public void AddsSuiteModifier()
+        {
+           
+        }
+        [Fact]
+        public void AddsRankModifier()
+        {
+   
+        }
+        [Fact]
+        public void JokersInCardList()
+        {
+
+        }
+        [Fact]
+        public void CardToScoreConversion()
+        {
+
+        }
+
+        [Fact]
+        public void InvalidCardSelectionException()
+        {
+
+
+        }
+    }
+}
